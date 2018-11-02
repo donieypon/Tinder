2cfc18e790bbceef705036acbfbe62f285feac5d
Donie Ypon <donieypon@rauls-macbook-pro-2.local>
2018-11-02 12:45:35 -0700

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Donie Ypon on 11/2/18.
+//  Copyright © 2018 Donie Ypon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
